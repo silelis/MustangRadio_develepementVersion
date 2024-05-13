f50dbef22fe79b67e7c261a88ba3b41c08188609
SileliS <silelis@users.noreply.github.com>
2024-05-13 22:10:30 +0200

    stm32 -fw- flash memory-mapped GOLDEN SAMPLE

--- 02_Firmware/02_STM32/qspi_test/Core/Src/main.c
+++ 02_Firmware/02_STM32/qspi_test/Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "string.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -50,7 +50,6 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MPU_Config(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -96,9 +95,6 @@ int main(void)
 
   /* USER CODE END 1 */
 
-  /* MPU Configuration--------------------------------------------------------*/
-  MPU_Config();
-
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
@@ -128,13 +124,19 @@ int main(void)
   uint8_t txbuf[] = "TEST";
   ret=W25Q128_OCTO_SPI_Init(&hospi1);
 
-  //ret=W25Q128_OSPI_Erase_Chip(&hospi1);
-  //ret=W25Q128_OSPI_Write(&hospi1,txbuf,0,sizeof(txbuf));
+  ret=W25Q128_OSPI_Erase_Chip(&hospi1);
+  printf("Erased\r\n");
+  ret=W25Q128_OSPI_Write(&hospi1,txbuf,0,sizeof(txbuf));
   ret=W25Q128_OSPI_Read(&hospi1, rxbuf, 0, 100);
   ret=W25Q128_OSPI_EnableMemoryMappedMode(&hospi1);
   HAL_Delay(1000);
 
-  memcpy(rxbuf, 0x90000000, 50);
+  /*__IO*/ uint8_t *mem_addr;
+  //mem_addr = (uint8_t *)(0x08010000);
+  mem_addr = (uint8_t *)(0x090000000);
+  uint8_t* ala = mem_addr;
+
+  memcpy(rxbuf, mem_addr, 8);
   printf("Hello world!!!\r\n");
 
 
@@ -227,35 +229,6 @@ void SystemClock_Config(void)
 
 
 /* USER CODE END 4 */
-
- /* MPU Configuration */
-
-void MPU_Config(void)
-{
-  MPU_Region_InitTypeDef MPU_InitStruct = {0};
-
-  /* Disables the MPU */
-  HAL_MPU_Disable();
-
-  /** Initializes and configures the Region and the memory to be protected
-  */
-  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-  MPU_InitStruct.BaseAddress = 0x0;
-  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
-  MPU_InitStruct.SubRegionDisable = 0x87;
-  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
-  /* Enables the MPU */
-  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-
-}
 
 /**
   * @brief  This function is executed in case of error occurrence.
