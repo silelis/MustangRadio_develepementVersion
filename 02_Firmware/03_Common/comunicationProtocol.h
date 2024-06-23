0ac46dec8de8cd52e4641c760578cec8c0bc9580
SileliS <silelis@users.noreply.github.com>
2024-06-23 22:29:56 +0200

    ESP32 STM32 - w communnication p[rotocle dodano pole CRC_xor

--- 02_Firmware/03_Common/comunicationProtocol.h
+++ 02_Firmware/03_Common/comunicationProtocol.h
@@ -45,8 +45,9 @@ typedef struct {
 
 typedef struct
 {	   	
-	uint8_t commandGroup; //aka I2C_COMMAND_GROUP
-	uint8_t dataSize;	//sizeof(data to send)
+	uint8_t	CRC_xor;		//xor checksum for i2c farame (commandGroup+dataSize+data)
+	uint8_t commandGroup;	//aka I2C_COMMAND_GROUP
+	uint8_t dataSize;		//sizeof(data to send)
 } i2cFrame_commonData;
 
 typedef struct {
