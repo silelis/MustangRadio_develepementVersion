ba2534c21d1f8a4b575fa521ef2b7f950a49a435
SileliS <silelis@users.noreply.github.com>
2024-08-22 21:53:00 +0200

    stm32 -fw- pierwsze próby smart devices

--- 02_Firmware/02_STM32/Radio_main/Debug/Core/Src/SileliS_code/subdir.mk
+++ 02_Firmware/02_STM32/Radio_main/Debug/Core/Src/SileliS_code/subdir.mk
@@ -5,6 +5,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../Core/Src/SileliS_code/SmartDevice.cpp \
 D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common/comunication_calculate_checksum.cpp \
 ../Core/Src/SileliS_code/esp32i2cComunicationDriver.cpp \
 ../Core/Src/SileliS_code/i2cEngine.cpp \
@@ -19,6 +20,7 @@ C_DEPS += \
 ./Core/Src/SileliS_code/printfRedirect.d 
 
 OBJS += \
+./Core/Src/SileliS_code/SmartDevice.o \
 ./Core/Src/SileliS_code/comunication_calculate_checksum.o \
 ./Core/Src/SileliS_code/esp32i2cComunicationDriver.o \
 ./Core/Src/SileliS_code/i2cEngine.o \
@@ -28,6 +30,7 @@ OBJS += \
 ./Core/Src/SileliS_code/tasksFunctions.o 
 
 CPP_DEPS += \
+./Core/Src/SileliS_code/SmartDevice.d \
 ./Core/Src/SileliS_code/comunication_calculate_checksum.d \
 ./Core/Src/SileliS_code/esp32i2cComunicationDriver.d \
 ./Core/Src/SileliS_code/i2cEngine.d \
@@ -37,9 +40,9 @@ CPP_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Core/Src/SileliS_code/comunication_calculate_checksum.o: D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common/comunication_calculate_checksum.cpp Core/Src/SileliS_code/subdir.mk
-	arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32H7A3xx -c -I../Core/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
 Core/Src/SileliS_code/%.o Core/Src/SileliS_code/%.su Core/Src/SileliS_code/%.cyclo: ../Core/Src/SileliS_code/%.cpp Core/Src/SileliS_code/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32H7A3xx -c -I../Core/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
+Core/Src/SileliS_code/comunication_calculate_checksum.o: D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common/comunication_calculate_checksum.cpp Core/Src/SileliS_code/subdir.mk
 	arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32H7A3xx -c -I../Core/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
 Core/Src/SileliS_code/i2c_slave_master_queueClass.o: D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common/i2c_slave_master_queueClass.cpp Core/Src/SileliS_code/subdir.mk
 	arm-none-eabi-g++ "$<" -mcpu=cortex-m7 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32H7A3xx -c -I../Core/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc -I../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32H7xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I"D:/!!!__GitHUB_repositories/MustangRadio_develepementVersion/02_Firmware/03_Common" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -o "$@"
@@ -49,7 +52,7 @@ Core/Src/SileliS_code/%.o Core/Src/SileliS_code/%.su Core/Src/SileliS_code/%.cyc
 clean: clean-Core-2f-Src-2f-SileliS_code
 
 clean-Core-2f-Src-2f-SileliS_code:
-	-$(RM) ./Core/Src/SileliS_code/comunication_calculate_checksum.cyclo ./Core/Src/SileliS_code/comunication_calculate_checksum.d ./Core/Src/SileliS_code/comunication_calculate_checksum.o ./Core/Src/SileliS_code/comunication_calculate_checksum.su ./Core/Src/SileliS_code/esp32i2cComunicationDriver.cyclo ./Core/Src/SileliS_code/esp32i2cComunicationDriver.d ./Core/Src/SileliS_code/esp32i2cComunicationDriver.o ./Core/Src/SileliS_code/esp32i2cComunicationDriver.su ./Core/Src/SileliS_code/i2cEngine.cyclo ./Core/Src/SileliS_code/i2cEngine.d ./Core/Src/SileliS_code/i2cEngine.o ./Core/Src/SileliS_code/i2cEngine.su ./Core/Src/SileliS_code/i2c_slave_master_queueClass.cyclo ./Core/Src/SileliS_code/i2c_slave_master_queueClass.d ./Core/Src/SileliS_code/i2c_slave_master_queueClass.o ./Core/Src/SileliS_code/i2c_slave_master_queueClass.su ./Core/Src/SileliS_code/menuItem.cyclo ./Core/Src/SileliS_code/menuItem.d ./Core/Src/SileliS_code/menuItem.o ./Core/Src/SileliS_code/menuItem.su ./Core/Src/SileliS_code/printfRedirect.cyclo ./Core/Src/SileliS_code/printfRedirect.d ./Core/Src/SileliS_code/printfRedirect.o ./Core/Src/SileliS_code/printfRedirect.su ./Core/Src/SileliS_code/tasksFunctions.cyclo ./Core/Src/SileliS_code/tasksFunctions.d ./Core/Src/SileliS_code/tasksFunctions.o ./Core/Src/SileliS_code/tasksFunctions.su
+	-$(RM) ./Core/Src/SileliS_code/SmartDevice.cyclo ./Core/Src/SileliS_code/SmartDevice.d ./Core/Src/SileliS_code/SmartDevice.o ./Core/Src/SileliS_code/SmartDevice.su ./Core/Src/SileliS_code/comunication_calculate_checksum.cyclo ./Core/Src/SileliS_code/comunication_calculate_checksum.d ./Core/Src/SileliS_code/comunication_calculate_checksum.o ./Core/Src/SileliS_code/comunication_calculate_checksum.su ./Core/Src/SileliS_code/esp32i2cComunicationDriver.cyclo ./Core/Src/SileliS_code/esp32i2cComunicationDriver.d ./Core/Src/SileliS_code/esp32i2cComunicationDriver.o ./Core/Src/SileliS_code/esp32i2cComunicationDriver.su ./Core/Src/SileliS_code/i2cEngine.cyclo ./Core/Src/SileliS_code/i2cEngine.d ./Core/Src/SileliS_code/i2cEngine.o ./Core/Src/SileliS_code/i2cEngine.su ./Core/Src/SileliS_code/i2c_slave_master_queueClass.cyclo ./Core/Src/SileliS_code/i2c_slave_master_queueClass.d ./Core/Src/SileliS_code/i2c_slave_master_queueClass.o ./Core/Src/SileliS_code/i2c_slave_master_queueClass.su ./Core/Src/SileliS_code/menuItem.cyclo ./Core/Src/SileliS_code/menuItem.d ./Core/Src/SileliS_code/menuItem.o ./Core/Src/SileliS_code/menuItem.su ./Core/Src/SileliS_code/printfRedirect.cyclo ./Core/Src/SileliS_code/printfRedirect.d ./Core/Src/SileliS_code/printfRedirect.o ./Core/Src/SileliS_code/printfRedirect.su ./Core/Src/SileliS_code/tasksFunctions.cyclo ./Core/Src/SileliS_code/tasksFunctions.d ./Core/Src/SileliS_code/tasksFunctions.o ./Core/Src/SileliS_code/tasksFunctions.su
 
 .PHONY: clean-Core-2f-Src-2f-SileliS_code
 
